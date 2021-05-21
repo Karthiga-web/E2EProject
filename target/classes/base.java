8ccfd7ef5151a312ea02a8bf18f9c8ea71b48045
Karthiga-web <kar07091995@gmail.com>
2021-05-21 10:23:04 -0700

    New Project

---
+++ target/classes/base.java
@@ -0,0 +1,56 @@
+package resources;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+public class base {
+
+	public WebDriver driver;
+	public Properties prop;
+
+	public WebDriver initializeDriver() throws IOException {
+
+		prop = new Properties();
+		FileInputStream fis = new FileInputStream(
+				"C:\\Users\\rahul\\E2EProject\\src\\main\\java\\resources\\data.properties");
+
+		prop.load(fis);
+		String browserName = prop.getProperty("browser");
+		System.out.println(browserName);
+
+		if (browserName.equals("chrome")) {
+			System.setProperty("webdriver.chrome.driver",
+					"C:\\Users\\grkar\\OneDrive\\Documents\\Selenium\\chromedriver_win32\\chromedriver.exe");
+			driver = new ChromeDriver();
+
+		} else if (browserName.equals("firefox")) {
+			System.setProperty("webdriver.gecko.driver", "C:\\Users\\grkar\\OneDrive\\Documents\\Selenium\\geckodriver-v0.29.1-win64\\geckodriver.exe");
+			driver = new FirefoxDriver();
+		} else if (browserName.equals("IE")) {
+			System.setProperty("webdriver.ie.driver", "C:\\Users\\grkar\\OneDrive\\Documents\\Selenium\\IEDriverServer_x64_3.150.1\\IEDriverServer.exe");
+		}
+
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		return driver;
+
+	}
+
+	public String getScreenShotPath(String testCaseName, WebDriver driver) throws IOException {
+		TakesScreenshot ts = (TakesScreenshot) driver;
+		File source = ts.getScreenshotAs(OutputType.FILE);
+		String destinationFile = System.getProperty("user.dir") + "\\reports\\" + testCaseName + ".png";
+		FileUtils.copyFile(source, new File(destinationFile));
+		return destinationFile;
+
+	}
+
+}
